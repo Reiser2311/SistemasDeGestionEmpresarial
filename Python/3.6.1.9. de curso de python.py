fa5bab75d32eada1ef64bb0611b25b2b57cec361
Iván Cuesta <148111204+Reiser2311@users.noreply.github.com>
2023-12-15 12:38:29 +0100

    Add files via upload

--- Python/3.6.1.9. de curso de python.py
+++ Python/3.6.1.9. de curso de python.py
@@ -1,11 +1,13 @@
-﻿my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+﻿my_list = [1, 2, 4, 5, 4, 1, 4, 2, 6, 2, 9]
 #
 # Escribe tu código aquí.
 #
 
+list = []
+
 for i in my_list:
-    if i in my_list:
-        del my_list[i]
+    if i not in list:
+        list.append(i)
 
 print("La lista con elementos únicos:")
-print(my_list)
+print(list)
