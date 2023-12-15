73a24bb9fbdf2fbe360a11fb08f6f7f3e5fd8af0
Iván Cuesta <148111204+Reiser2311@users.noreply.github.com>
2023-12-15 10:57:35 +0100

    Add files via upload

---
+++ Python/3.2.1.15 Laboratorio de Python.py
@@ -0,0 +1,12 @@
+﻿c0 = int(input())
+pasos = 0
+
+while c0 > 1:
+    if c0 % 2 == 0:
+        c0 = c0 // 2
+    else:
+        c0 = c0 * 3 + 1
+    print(c0)
+    pasos += 1
+    
+print("Pasos: ", pasos)
